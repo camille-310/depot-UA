ab52ca20f9970269d3d1cbe2f70bde56d010eead
camille-310 <camille.cesbron9@gmail.com>
2024-11-30 14:25:53 +0100

    ajout de type_contenu

--- fonctions.py
+++ fonctions.py
@@ -10,3 +10,27 @@ def telecharger_url(url,dossier):
     with open(chemin_fichier, 'wb') as doc:
                 doc.write(reponse.content)
     return chemin_fichier
+
+def type_contenu(url):
+    try:
+        # Envoyer une requête HEAD pour récupérer les en-têtes
+        response = requests.head(url)
+        
+        # Vérifier si la requête a réussi
+        if response.status_code == 200:
+            # Récupérer le type du contenu
+            content_type = response.headers.get('Content-Type', '').lower()
+            
+            # Déterminer le type en fonction de Content-Type
+            if "application/pdf" in content_type:
+                return "PDF"
+            elif "application/epub+zip" in content_type:
+                return "EPUB"
+            elif "text/html" in content_type:
+                return "HTML"
+            else:
+                return f"Autre type"
+        else:
+            return f"Échec de la requête. Statut code : {response.status_code}"
+    except requests.RequestException as e:
+        return f"Erreur de connexion : {e}"
