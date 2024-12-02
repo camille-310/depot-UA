640349c849279d5227d96c8e75816cf80d308e41
camille-310 <camille.cesbron9@gmail.com>
2024-12-02 19:59:18 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -13,30 +13,6 @@ def telecharger_url(url,dossier):
                 doc.write(reponse.content)
     return chemin_fichier
 
-def type_contenu(url):
-    try:
-        # Envoyer une requête HEAD pour récupérer les en-têtes
-        response = requests.head(url)
-        
-        # Vérifier si la requête a réussi
-        if response.status_code == 200:
-            # Récupérer le type du contenu
-            content_type = response.headers.get('Content-Type', '').lower()
-            
-            # Déterminer le type en fonction de Content-Type
-            if "application/pdf" in content_type:
-                return "PDF"
-            elif "application/epub+zip" in content_type:
-                return "EPUB"
-            elif "text/html" in content_type:
-                return "HTML"
-            else:
-                return f"Autre type"
-        else:
-            return f"Échec de la requête. Statut code : {response.status_code}"
-    except requests.RequestException as e:
-        return f"Erreur de connexion : {e}"
-
 def _generer_pdf(self, fichier, contenu):
     # Taille de la page (Letter/A4)
     width, height = 612, 792
